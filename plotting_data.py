37355172f6214a0a3c6b9549cb4ae02b367a413f
Zach Zemokhol <45083180+SoyStillGood@users.noreply.github.com>
2019-11-08 18:55:11 -0500

    First commit, pairsplots done

---
+++ plotting_data.py
@@ -0,0 +1,23 @@
+import argparse
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", dest="file", help="input data file")
+
+args = parser.parse_args()
+file = args.file
+
+diabetes = pd.read_csv(filepath_or_buffer=file, sep=' ', header=0)
+print(diabetes)
+
+os.makedirs('viz-plots/viz-homework', exist_ok=True)
+
+
+
+pairs_plot = sns.pairplot(diabetes)
+plt.savefig('viz-plots/viz-homework/pairs.png')
+
